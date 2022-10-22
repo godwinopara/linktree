8b601a8c7b4f4044f6353f52e335162dba76c949
Godwin Opara <godwinopara62@yahoo.com>
2022-10-22 18:19:02 +0100

    styled share link

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,18 @@
 import profileImg from "../../images/profile__img.svg";
 import "./profile.css";
+import shareBtn from "../../images/share.svg";
 
 const Profile = () => {
   return (
     <section className="profile">
+      <div className="share_btn">
+        {/* Tool Tip */}
+        <div className="tooltip">
+          <span className="tooltip-txt">Share Link</span>
+        </div>
+        {/*  */}
+        <img src={shareBtn} alt="share icon" />
+      </div>
       <div className="profile-img">
         <img src={profileImg} alt="" />
       </div>
