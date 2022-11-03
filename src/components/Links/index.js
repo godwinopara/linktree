59842aa7807e4295f3fa5efc5cbbcfdc1eb8a8d7
Godwin Opara <godwinopara62@yahoo.com>
2022-11-03 12:11:41 +0100

    updated the links file by putting the links data into a new folder called data

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,57 +1,20 @@
 import "./link.css";
+import { linksData } from "../../data/linkData";
 
 const Links = () => {
-  const links = [
-    {
-      id: "twitter",
-      name: "Twitter Link",
-      url: "https://twitter.com/godwinopara12",
-      subText: "",
-    },
-    {
-      id: "btn__zuri",
-      name: "Zuri Team",
-      url: "https://training.zuri.team/",
-    },
-    {
-      id: "books",
-      name: "Zuri Books",
-      url: "http://books.zuri.team",
-      subText:
-        "Look no further on where to get the best books on design and coding",
-    },
-    {
-      id: "book__python",
-      name: "Python Books",
-      url: "https://books.zuri.team/python-for-beginners?ref_id=godwin",
-      subText: "Get Top notch detailed python books here",
-    },
-    {
-      id: "pitch",
-      name: "Background Check For Coders",
-      url: "https://background.zuri.team",
-      subText:
-        "Not sure about your programming skills, we can do a background check for you",
-    },
-    {
-      id: "book__desing",
-      name: "Design Books",
-      url: "https://books.zuri.team/design-rules",
-      subText:
-        "Get the best design book that will help you create truly professional designs",
-    },
-  ];
   return (
     <div className="links-wrapper">
-      {links.map((link, id) => {
+      {linksData.map((link, id) => {
         return (
-          <div className={`link ${link.id}`}>
-            <a key={id} href={link.url} id={link.id}>
+          <div key={id} className={`link ${link.id}`}>
+            <a href={link.url} id={link.id}>
               {link.name}
             </a>
 
             {link.subText && (
-              <span class={`subtext subText-${link.id}`}>{link.subText}</span>
+              <span className={`subtext subText-${link.id}`}>
+                {link.subText}
+              </span>
             )}
           </div>
         );
---
+++ src/data/linkData.js
@@ -0,0 +1,45 @@
+export const linksData = [
+  {
+    id: "twitter",
+    name: "Twitter Link",
+    url: "https://twitter.com/godwinopara12",
+    subText: "",
+  },
+  {
+    id: "btn__zuri",
+    name: "Zuri Team",
+    url: "https://training.zuri.team/",
+  },
+  {
+    id: "books",
+    name: "Zuri Books",
+    url: "http://books.zuri.team",
+    subText:
+      "Look no further on where to get the best books on design and coding",
+  },
+  {
+    id: "book__python",
+    name: "Python Books",
+    url: "https://books.zuri.team/python-for-beginners?ref_id=godwin",
+    subText: "Get Top notch detailed python books here",
+  },
+  {
+    id: "pitch",
+    name: "Background Check For Coders",
+    url: "https://background.zuri.team",
+    subText:
+      "Not sure about your programming skills, we can do a background check for you",
+  },
+  {
+    id: "book__desing",
+    name: "Design Books",
+    url: "https://books.zuri.team/design-rules",
+    subText:
+      "Get the best design book that will help you create truly professional designs",
+  },
+  {
+    id: "contact",
+    name: "Contact Me",
+    url: "/contact",
+  },
+];
