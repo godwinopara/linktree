ea2d3613a46fef553b814472bc6876f2930e9721
Godwin Opara <godwinopara62@yahoo.com>
2022-11-03 16:42:07 +0100

    styles: styled the contact page to be responsive on mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,44 @@
 const Contact = () => {
   return (
-    <header>
+    <>
+      <header className="pt-24 px-6 mb-12">
+        <div>
+          <h1 className="text-4xl font-semibold mb-6">Contact Me</h1>
+          <p className="text-lg">
+            Hi there, contact me to ask me about anything you have in mind.
+          </p>
+        </div>
+      </header>
       <main>
-        <form action="">
+        <form className="px-6 mb-24">
           <div>
-            <div>
-              <label htmlFor="firstname">First Name</label>
+            <div className="flex flex-col mb-9">
+              <label htmlFor="firstname" className="">
+                First Name
+              </label>
               <input
                 type="text"
                 name="firstname"
                 placeholder="Enter your first name"
+                id="first_name"
               />
             </div>
-            <div>
-              <label htmlFor="lastname">Last Name</label>
+            <div className="flex flex-col mb-9">
+              <label htmlFor="lastname" className="">
+                Last Name
+              </label>
               <input
                 type="text"
                 name="lastname"
                 placeholder="Enter your last name"
+                id="last_name"
               />
             </div>
           </div>
-          <div>
-            <label htmlFor="email">Email</label>
+          <div className="flex flex-col mb-9">
+            <label htmlFor="email" className="">
+              Email
+            </label>
             <input
               type="email"
               name="email"
@@ -30,8 +46,10 @@ const Contact = () => {
               placeholder="youremail@email.com"
             />
           </div>
-          <div>
-            <label htmlFor="message">Message</label>
+          <div className="flex flex-col mb-9">
+            <label htmlFor="message" className="text-sm font-medium">
+              Message
+            </label>
             <textarea
               name="message"
               id="message"
@@ -40,9 +58,27 @@ const Contact = () => {
               placeholder="Send me message and I will reply as soon as possible"
             ></textarea>
           </div>
+          <div className="mb-9 flex items-start justify-center">
+            <input
+              type="checkbox"
+              name="checkbox_agree"
+              id="checkbox_agree"
+              className="mt-1"
+            />
+            <label htmlFor="checkbox_agree" className="text-sm ml-3">
+              You agree to providing your date to Godwin Opara who may contact
+              you
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-750 border border-blue-750 font-semibold text-base h-12  w-full text-white rounded-xl"
+          >
+            Send message
+          </button>
         </form>
       </main>
-    </header>
+    </>
   );
 };
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-750": "#1570EF",
+      },
+
+      spacing: {
+        90: "21.44rem", // 343px
+      },
+    },
+  },
+  plugins: [],
+};
