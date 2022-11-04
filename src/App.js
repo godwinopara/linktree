1c8687d32d408836e4a4676112e688dab369db7f
Godwin Opara <godwinopara62@yahoo.com>
2022-11-04 13:50:08 +0100

    refactor: added netlify config

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact";
-import HomeTest from "./pages/HomeTest";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/home" element={<HomeTest />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </BrowserRouter>
